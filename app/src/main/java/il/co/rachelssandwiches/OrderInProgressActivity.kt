5fd276629625600515e35a5c2a2e68f0b9d46293
אילון <eiloncap@gmail.com>
2021-05-29 20:02:06 +0300

    Added activity_order_in_progress layout

--- app/src/main/java/il/co/rachelssandwiches/OrderInProgressActivity.kt
+++ app/src/main/java/il/co/rachelssandwiches/OrderInProgressActivity.kt
@@ -3,9 +3,9 @@ package il.co.rachelssandwiches
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class InProgressActivity : AppCompatActivity() {
+class OrderInProgressActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_in_progress)
+        setContentView(R.layout.activity_order_in_progress)
     }
 }
